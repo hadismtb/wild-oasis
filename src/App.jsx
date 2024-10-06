5dc81d5f4d700a6c81449b06fd7fc2b527a8a143
Hadis <hadisematloob@yahoo.com>
2024-10-06 15:27:52 +0330

    create app sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
-            <Route path="user" element={<Users />} />
+            <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
