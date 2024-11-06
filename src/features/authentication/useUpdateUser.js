cabfc85a3204e175005af17a3625a31bb1c77fd7
Hadis <hadisematloob@yahoo.com>
2024-11-06 22:26:01 +0330

    add "update user data" and "change password" option

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser } from "../../services/apiAuth.js";
+import { toast } from "react-toast";
+
+function useUpdateUser() {
+  const queryClient = useQueryClient();
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+
+    onSuccess: ({ user }) => {
+      toast.success("User account successfully updated.");
+      queryClient.setQueryData(["user"], user);
+      // queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { updateUser, isUpdating };
+}
+
+export default useUpdateUser;
